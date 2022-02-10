be9e3b059e27630504d0c5146a213c47eaddecd9
Ben Meese <btmeese@geneva.edu>
2022-02-10 15:49:01 -0500

    Deliverable 2

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,4 +1,24 @@
+import time
+from math import sqrt
+start_time = time.time()
+
 
 def PrimeNumberCalculation(n):
-    prime = [True for num in range(2, n)]
+    if(n > 2):
+        prime = [*range(2, n)]
+        x = 2
+        while (x * x < n):
+            if (x < sqrt(n)):
+                for i in range(x * x, n, x):
+                    prime.remove(i)
+            x += 1
     
+        number_of_elements = len(prime)
+        print("Total number of prime numbers less than provided limit: ", number_of_elements)
+    else:
+        print("Please enter a limit that is greater than 2.")
+
+limit = int(input("Enter an upper limit for the prime number search: "))
+PrimeNumberCalculation(limit)
+
+print("Program Run Time: %s seconds" % (time.time() - start_time))
